e6fc37d2b12aeda31f5277071439b0772e249cdd
Derek R <saa.derek.r@gmail.com>
2020-08-10 14:32:05 -0400

    Removed old commented code.

--- commands/animals.js
+++ commands/animals.js
@@ -1,18 +1,6 @@
 const axios = require('axios');
 const { isEmpty } = require('lodash');
 
-// module.exports = async (message) => {
-//     try {
-//         const { data: { joke } } = await axios.get('https://icanhazdadjoke.com/', { 
-//             headers: { 'Accept': 'application/json' } 
-//         });
-//         message.reply(joke);
-//     } catch (error) {
-//         console.log(error);
-//         message.reply('There was an error.');
-//     }
-// };
-
 const getAnimal = async (msg, url, transformer) => {
     msg.edit(':arrows_counterclockwise:');
     let data;
